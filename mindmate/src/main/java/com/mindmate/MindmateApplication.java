f1ba9316b4573f26b04af053455d3edb52c57fda
FarooquiAun <farooqui.aun@fyntune.com>
2025-08-12 14:51:51 +0530

    inital commit

---
+++ mindmate/src/main/java/com/mindmate/MindmateApplication.java
@@ -0,0 +1,13 @@
+package com.mindmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindmateApplication.class, args);
+	}
+
+}
---
+++ mindmate/src/test/java/com/mindmate/MindmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
